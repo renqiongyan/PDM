7ea74c0fd30538716dc048dfd5ecace146c5b796
任琼燕 <2423947238@qq.com>
2019-06-25 14:16:14 +0800

    七七：更改worker部分py文件:商品信息

--- application/shopowner/views.py
+++ application/shopowner/views.py
@@ -57,21 +57,3 @@ def list_goods():
 def list_trans(id):
     trans = Transport.select().where(Transport.id == id)
     return render_template('senior/list_trans.html', trans=trans)
-
-
-#
-# @bp_user.route('/add_user',methods=['GET', 'POST'])
-# def add_user():
-#     form = AddForm()
-#     if form.validate_on_submit():
-#         User.create(
-#             username=form.username.data,
-#             password = generate_password_hash(form.password.data),
-#             nickname = form.nickname.data,
-#             gender = form.gender.data,
-#             role = form.role.data,
-#         )
-#         flash('增加成功')
-#         return redirect(url_for('bp_user.list_users'))
-#
-#     return render_template('user/add_user.html', form=form)
